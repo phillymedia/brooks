fe423f16d3e50691b69c1637b90ff6e947f9fea5
Garland <garlandpotts1@gmail.com>
2017-08-08 16:15:15 -0400

    Bookmark updates

--- app/js/main.js
+++ app/js/main.js
@@ -4,13 +4,11 @@ require("./modernizr");
 
 $(document).ready(function() {
 
-
     $(".backgroundImage").addClass("fadeImage");
     $(".overlayText").addClass("fadeHeader");
 
     $(".cssgrid .gridSection").css("top", $(".header").height() + 4 + "px");
-    $(".addBookmark").css("top", $(".header").height() + 15 + "px");
-    $("body").append("<div class='addBookmark'><i class='fa fa-bookmark' aria-hidden='true'></i> Save my spot for later</div>");
+    $("body").append("<div class='addBookmark'><i class='fa fa-bookmark' aria-hidden='true'></i> <span id='savespot'>Save my spot for later</span></div>");
 
     if (Cookies('prev_scroll_position')) {
         $("body").append("<div class='cookie-load'><div class='cookie-wrap'><div class='closeWindow'>X</div><div class='cookie-text'>Welcome back. Would you like to continue where you left off?</div><div class='continueButtons'><div id='cbtn-yes' class='cbtn'>Yes, start where I left off</div><div class='cbtn' id='cbtn-no'>No, start at the beginning</div></div></div></div>");
@@ -54,19 +52,20 @@ $(document).ready(function() {
 
     function checkScroll(event) {
 
-        // setTimeout(function() {
-        //     Cookies('prev_scroll_position', scroll_positon, {
-        //         expires: 7,
-        //         path: '/'
-        //     });
-        // }, 10);
+
+        setTimeout(function() {
+            Cookies('prev_scroll_position', scroll_positon, {
+                expires: 7,
+                path: '/'
+            });
+        }, 10);
 
         $("p").each(function() {
             var $element = $(this);
             var element_top_position = $element.offset().top;
             var window_top_position = $(window).scrollTop();
-            if (element_top_position - $(".header").height() <= window_top_position) {
-                scroll_positon = element_top_position - $(".header").height() - 15;
+            if (element_top_position - $(window).height()*.4 <= window_top_position) {
+                scroll_positon = Number(element_top_position) - $(".header").height() - 15;
             }
         })
 
@@ -81,28 +80,48 @@ $(document).ready(function() {
         })
     }
     $(".addBookmark").on("click", function() {
-        console.log(scroll_positon);
+
         Cookies('prev_scroll_position', scroll_positon, {
             expires: 7,
             path: '/'
         });
 
-        $.ajax({
-            url: "https://docs.google.com/forms/d/1MydBN5zjxMnpejnsa0whIh2tXbm0Cdzh6leKR9SupN4/formResponse",
-            data: {
-                "entry.2044532047": scroll_positon
-            },
-            type: "POST",
-            dataType: "xml",
-            statusCode: {
-                0: function() {
-                    //Success message
-                },
-                200: function() {
-                    //Success Message
-                }
+        $(".bookmark").remove();
+
+        $("#savespot").text("Bookmark saved");
+
+        var cookieSet = Number(scroll_positon) + $(".header").height() + 20;
+        // $("body").append("<div class='bookmark'><i class='fa fa-bookmark' aria-hidden='true'></i>Bookmarked position</div>");
+        // $(".bookmark").css("top", cookieSet + "px");
+
+
+        $("p").each(function() {
+            var $element = $(this);
+            var element_top_position = $element.offset().top;
+            var window_top_position = $(window).scrollTop();
+            if (element_top_position - $(".header").height() - 15 == scroll_positon) {
+            $element.prepend("<div class='bookmarkPosition'><i class='fa fa-bookmark' aria-hidden='true'></i>Bookmarked position</div>")
             }
-        });
+        })
+
+
+        // $.ajax({
+        //     url: "https://docs.google.com/forms/d/1MydBN5zjxMnpejnsa0whIh2tXbm0Cdzh6leKR9SupN4/formResponse",
+        //     data: {
+        //         "entry.2044532047": scroll_positon
+        //     },
+        //     type: "POST",
+        //     dataType: "xml",
+        //     statusCode: {
+        //         0: function() {
+        //             //Success message
+        //         },
+        //         200: function() {
+        //             //Success Message
+        //         }
+        //     }
+        // });
     })
+    $(".addBookmark").css("top", ($(".header").height() + 15 + "px"));
     $(window).scroll(_.throttle(checkScroll, 1000));
 });
